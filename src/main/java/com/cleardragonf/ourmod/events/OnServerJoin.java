27d6359875f13404d4a900020c5f4fd6f402facf
Cleardragonf <chrism339@yahoo.com>
2020-06-12 23:56:47 -0500

    •  Added on Player join giving them a book

--- src/main/java/com/cleardragonf/ourmod/events/OnServerJoin.java
+++ src/main/java/com/cleardragonf/ourmod/events/OnServerJoin.java
@@ -2,7 +2,13 @@ package com.cleardragonf.ourmod.events;
 
 import com.cleardragonf.ourmod.OurMod;
 
+import com.cleardragonf.ourmod.init.ItemInitNew;
+import com.cleardragonf.ourmod.tileentity.EssenceCollectorTileEntity;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.util.text.TranslationTextComponent;
+import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -14,5 +20,10 @@ public class OnServerJoin
 	@SubscribeEvent
 	public static void  OnServerJoin(PlayerEvent.PlayerLoggedInEvent event) {
 		event.getPlayer().sendMessage(new TranslationTextComponent("hello"));
+
+
+		ItemStack item = new ItemStack( ItemInitNew.OUR_BOOK.get());
+		ItemEntity entity = new ItemEntity(event.getPlayer().world, event.getPlayer().getPosX() + .5, event.getPlayer().getPosY(), event.getPlayer().getPosZ() + .5, item);
+		event.getPlayer().world.addEntity(entity);
 	}
 }
